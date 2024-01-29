d97011aeec807c4eca1eb432372ab16fc8373389
SarikAnwar01 <sarikanwar007@gmail.com>
2024-01-29 17:07:49 +0545

    setup and auth for the dashboard app

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,13 @@
+import {Avatar as AntdAvatar} from "antd";
+const CustomAvatar = () => {
+  return (
+    <AntdAvatar 
+    alt={"Sarik Anwar"}
+    size="small"
+    style={{backgroundColor:'#87d068'}}>
+        SA
+    </AntdAvatar>
+  )
+}
+
+export default CustomAvatar
