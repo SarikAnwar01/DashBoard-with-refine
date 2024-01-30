b5741fa384333fb706a023dafd15dc19e45c3d02
SarikAnwar01 <sarikanwar007@gmail.com>
2024-01-30 22:34:54 +0545

    grapql mutations and queries and its types

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,13 +1,25 @@
-import {Avatar as AntdAvatar} from "antd";
-const CustomAvatar = () => {
+import { Avatar as AntdAvatar } from "antd";
+import { AvatarProps } from "antd/lib";
+type Props = AvatarProps & {
+  name?: string;
+};
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
-    <AntdAvatar 
-    alt={"Sarik Anwar"}
-    size="small"
-    style={{backgroundColor:'#87d068'}}>
-        SA
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{
+        backgroundColor: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style,
+      }}
+      // {...rest}
+    >
+      {name}
     </AntdAvatar>
-  )
-}
+  );
+};
 
-export default CustomAvatar
+export default CustomAvatar;
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,20 +1,28 @@
 import { Button, Popover } from "antd";
 import CustomAvatar from "../custom-avatar";
-
+import { useGetIdentity } from "@refinedev/core";
+import type { User } from "@/graphql/schema.types";
 
 const CurrentUser = () => {
+  const { data: user } = useGetIdentity<User>();
+
   return (
     <>
-     <Popover
-     placement="bottomRight"
-     trigger="click"
-     overlayInnerStyle={{padding:0}}
-     overlayStyle={{zIndex:999}}
-     >
-       <CustomAvatar/>
-     </Popover>
+      <Popover
+        placement="bottomRight"
+        trigger="click"
+        overlayInnerStyle={{ padding: 0 }}
+        overlayStyle={{ zIndex: 999 }}
+      >
+        <CustomAvatar
+          name={user?.name || "NA"}
+          src={user?.avatarUrl}
+          size="default"
+          style={{ cursor: "pointer" }}
+        />
+      </Popover>
     </>
-  )
-}
+  );
+};
 
 export default CurrentUser;
